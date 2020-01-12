a6c4c1f24b103d071d05875bf8fe314dff7482d9
Hrushikesh Datar <59263874+hrudat16@users.noreply.github.com>
2020-01-13 00:49:22 +0530

    Add files via upload

--- bhav_download.py
+++ bhav_download.py
@@ -32,11 +32,11 @@ M = ustr.upper()
 D1 =  yesterday.day
 
 if D1 < 9:
-    str1 = "https://www.nseindia.com/content/historical/EQUITIES/"+Y+"/"+M+"/cm0"+D+M+Y+"bhav.csv.zip"
+    str1 = "https://www1.nseindia.com/content/historical/EQUITIES/"+Y+"/"+M+"/cm0"+D+M+Y+"bhav.csv.zip"
     
 else:
-    str1 = "https://www.nseindia.com/content/historical/EQUITIES/"+Y+"/"+M+"/cm"+D+M+Y+"bhav.csv.zip"
-    
+    str1 = "https://www1.nseindia.com/content/historical/EQUITIES/"+Y+"/"+M+"/cm"+D+M+Y+"bhav.csv.zip"
+
 # Extracting downloaded file
 udownload = requests.get(str1,stream=True)
 zfile = zipfile.ZipFile(io.BytesIO(udownload.content))
@@ -44,3 +44,25 @@ zfile.extractall()
 zfile.extractall(path="E:/Bhavcopies/")
 file=max(glob.glob("E:/Bhavcopies/*"), key=os.path.getctime)
 
+#Pickup latest file and analysis code starts here
+if D1 < 9:
+    latest_file = glob.glob("E:/Bhavcopies/cm0"+D+M+Y+"bhav.csv.zip")
+
+else:
+    latest_file = glob.glob("E:/Bhavcopies/cm"+D+M+Y+"bhav.csv.zip")
+
+cf =  pd.read_csv ("cm10JAN2020bhav.csv", usecols = ['SYMBOL','HIGH','LOW','CLOSE'])
+
+df =  pd.DataFrame(cf)
+
+df['PIVOT POINT'] = (df['HIGH']+df['LOW']+df['CLOSE'])/3
+df['R1'] = (2*df['PIVOT POINT'])-df['LOW']
+df['R2'] = df['PIVOT POINT'] - (df['LOW']-df['CLOSE'])
+df['R3'] = df['HIGH']+(2*(df['PIVOT POINT']-df['LOW']))
+df['S1'] = (2 * df['PIVOT POINT']) - df['HIGH'] 
+df['S2'] = df['PIVOT POINT'] - (df['HIGH']-df['LOW'])
+df['S3'] = df['LOW']+(2*(df['HIGH']-df['PIVOT POINT']))
+
+final_excel = df.round()
+
+final_excel.to_excel(r'E:\bhavcopies\Final_daily_data_'+D+M+Y+'.xlsx')
